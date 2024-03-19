f9f31ea42e3c782bc7b73939eb9977ab32aa25e7
fragoskee1 <www.vinit8249@gmail.com>
2024-03-19 21:21:32 +0530

    work

--- src/App.js
+++ src/App.js
@@ -1,44 +1,55 @@
 import './App.css';
-
 function App() {
   return (
     <>
- <nav className="navbar navbar-expand-lg bg-body-tertiary">
-  <div className="container-fluid">
-    <a className="navbar-brand" href="/">Navbar scroll</a>
-    <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarScroll" aria-controls="navbarScroll" aria-expanded="false" aria-label="Toggle navigation">
-      <span className="navbar-toggler-icon"></span>
-    </button>
-    <div className="collapse navbar-collapse" id="navbarScroll">
-      <ul className="navbar-nav me-auto my-2 my-lg-0 navbar-nav-scroll" style="--bs-scroll-height: 100px;">
-        <li className="nav-item">
-          <a className="nav-link active" aria-current="page" href="/">Home</a>
-        </li>
-        <li className="nav-item">
-          <a className="nav-link" href="/">Link</a>
-        </li>
-        <li className="nav-item dropdown">
-          <a className="nav-link dropdown-toggle" href="/" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-            Link
-          </a>
-          <ul className="dropdown-menu">
-            <li><a className="dropdown-item" href="/">Action</a></li>
-            <li><a className="dropdown-item" href="/">Another action</a></li>
-            <li><hr className="dropdown-divider"/></li>
-            <li><a className="dropdown-item" href="/">Something else here</a></li>
-          </ul>
-        </li>
-        <li className="nav-item">
-          <a className="nav-link disabled" aria-disabled="true">Link</a>
-        </li>
-      </ul>
-      <form className="d-flex" role="search">
-        <input className="form-control me-2" type="search" placeholder="Search" aria-label="Search"/>
-        <button className="btn btn-outline-success" type="submit">Search</button>
-      </form>
+<div className="container-fluid">
+  <div className="row layout">
+    <div className="navbar col-sm-2 navbar-expand-lg">
+      <div className="items">
+        <img src="/logo.png" alt="img" className="logo img-fluid" />
+        <li className="Dashboard" href="/">Dashboard</li>
+        <li className="Booking" href="/">Booking</li>
+        <li className="Analytics" href="/">Analytics</li>
+        <li className="Passengers" href="/">Passengers</li>
+        <li className="Cars" href="/">Cars</li>
+        <li className="Drivers" href="/">Drivers</li>
+        <li className="App Settings" href="/">App Settings</li>
+        <li className="Notifications" href="/">Notifications</li>
+        <li className="Reports" href="/">Reports</li>
+        <li className="Vendor" href="/">Vendor</li>
+        <li className="Admin" href="/">Admin</li>
+        <li className="logout" href="/"><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentcolor" className="bi bi-box-arrow-right logouticon" viewBox="0 0 16 16">
+        <path fill-rule="evenodd" d="M10 12.5a.5.5 0 0 1-.5.5h-8a.5.5 0 0 1-.5-.5v-9a.5.5 0 0 1 .5-.5h8a.5.5 0 0 1 .5.5v2a.5.5 0 0 0 1 0v-2A1.5 1.5 0 0 0 9.5 2h-8A1.5 1.5 0 0 0 0 3.5v9A1.5 1.5 0 0 0 1.5 14h8a1.5 1.5 0 0 0 1.5-1.5v-2a.5.5 0 0 0-1 0z"/>
+        <path fill-rule="evenodd" d="M15.854 8.354a.5.5 0 0 0 0-.708l-3-3a.5.5 0 0 0-.708.708L14.293 7.5H5.5a.5.5 0 0 0 0 1h8.793l-2.147 2.146a.5.5 0 0 0 .708.708z"/>
+        </svg>Logout</li>
+      </div>
+    </div>
+    <div className="details col-sm-10 bg-secondary">
+      <div className="header container">
+        <div className="row">
+          <div className="vendor col-sm-10">
+            Vendor
+          </div>
+          <div className="id col-sm-2 bg-light">
+            <img src="/logo.png" alt="image" className="user img-fluid" />
+            Hello, Anita
+            <div className="dropdown">
+              <li class="nav-item dropdown">
+                <a class="nav-link dropdown-toggle" href="/" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                </a>
+                <ul class="dropdown-menu">
+                  <li><a class="dropdown-item" href="/">Action</a></li>
+                  <li><a class="dropdown-item" href="/">Another action</a></li>
+                  <li><a class="dropdown-item" href="/">Something else here</a></li>
+                </ul>
+              </li>
+          </div>
+          </div>
+        </div>
+       </div>
     </div>
   </div>
-</nav>
+</div>
     </>
   );
 }
